106d2a194138f4237fcace8a2ba2f33c7529ea0a
Grayson Crozier <graysoncrozier40@gmail.com>
2023-12-04 18:22:45 -0500

    Mock FA vector icons

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,3 +1,10 @@
 import '@testing-library/jest-native/extend-expect';
 
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+
+jest.mock('@expo/vector-icons', () => {
+    return {
+        FontAwesome: 'FontAwesomeMock',
+        // Add other icons or sets as needed
+    };
+});
