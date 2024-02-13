83b55b4b03f5ce5ad63f07c88876e5d23cdb4134
Grayson Crozier <graysoncrozier40@gmail.com>
2024-02-12 23:23:02 -0500

    Add PFAS results screen

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -3,5 +3,21 @@ import HomeScreen from './homeScreen';
 import Register from "./Register";
 import ProfileScreen from './ProfileScreen';
 import EditProfileScreen from './EditProfileScreen';
+import AddTestResultsScreen from './AddTestResultsScreen';
+import ViewResultsScreen from './ViewResultsScreen';
+import ViolationsScreen from './ViolationsScreen';
+import PfasScreen from './PfasScreen';
+import MapScreen from './MapScreen';
 
-export { Login, HomeScreen, Register, ProfileScreen, EditProfileScreen };
+export { 
+    Login, 
+    HomeScreen, 
+    Register, 
+    ProfileScreen, 
+    EditProfileScreen,  
+    AddTestResultsScreen,
+    ViewResultsScreen,
+    ViolationsScreen,
+    PfasScreen,
+    MapScreen
+};
